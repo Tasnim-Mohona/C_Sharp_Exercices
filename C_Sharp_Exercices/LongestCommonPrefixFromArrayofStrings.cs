e0da155d3745ec874cfc08bf3131d0b8eb5f8e06
qmohona <qmohona@gmail.com>
2024-09-30 16:57:00 +0600

    C#: Find the longest common prefix from an array of strings

---
+++ C_Sharp_Exercices/LongestCommonPrefixFromArrayofStrings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Exercices
+{
+    class LongestCommonPrefixFromArrayofStrings
+    {
+
+
+
+        public string test(string[] arr_strings)
+        {
+            // Checking for edge cases :  empty array or an array containing an empty string
+            if (arr_strings.Length == 0 || Array.IndexOf(arr_strings, "") != -1)
+                return "";
+
+
+            // Initilizing "result" to the first string in the array
+            string result = arr_strings[0];
+            int i = result.Length;
+
+            // Looping through each work in the array to find the longest common prefix
+            foreach (string word in arr_strings)
+            {
+                int j = 0;
+
+                // Comparing characters at each position in the words
+                foreach (char c in word)
+                {
+                    // Breaking if characters do nor match or index 'j' exceeds the length of the result
+                    if (j >= i || result[j] != c)
+                        break;
+                    j += 1;
+                }
+
+                // Updating 'i' with the minimum value between 'i' and 'j'
+                i = Math.Min(i, j);
+            }
+
+            // Returning the longest common prefix based on 'i'
+            return result.Substring(0, i);
+        }
+
+    }
+}
--- C_Sharp_Exercices/Main.cs
+++ C_Sharp_Exercices/Main.cs
@@ -10,6 +10,13 @@ namespace C_Sharp_Exercices
             SwapNumbers swapNumbers = new SwapNumbers();
             swapNumbers.Swap();
 
+            // Initializing string arrays with different sets of strings
+            LongestCommonPrefixFromArrayofStrings common = new LongestCommonPrefixFromArrayofStrings();
+
+            string[] arr_strings1 = { "Pandas", "Panther" };
+            Console.Out.WriteLine("Original Strings: " + $"{ string.Join(", ", arr_strings1)}");
+            Console.WriteLine("Longest common prefix from the said array of strings: " + common.test(arr_strings1));
+
         }
     }
 }
